6f5f4c2a45c66436c2359f36efd98c2287f7e734
manten120 <hamaw8888@gmail.com>
2022-01-12 08:58:50 +0900

    Format the code

--- app/express/src/routes/index.ts
+++ app/express/src/routes/index.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
-router.get("/", (_req, res, _next) => {
-  res.send("routes/index");
+router.get('/', (_req, res, _next) => {
+  res.send('routes/index');
 });
 
 export { router as indexRouter };
