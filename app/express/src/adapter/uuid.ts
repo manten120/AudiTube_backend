09b53bf36b8db433fd150dec42852bb8e45b030a
manten120 <hamaw8888@gmail.com>
2022-01-16 18:25:52 +0900

    コメントを追記

--- app/express/src/adapter/uuid.ts
+++ app/express/src/adapter/uuid.ts
@@ -1,6 +1,8 @@
 import crypto from 'crypto';
 
 export const createUUID = () => {
+  // crypto.randomUUID()はnpm packageのuuidより3倍早くuuidを生成できる
+  // 参考 crypto.randomUUID is three times faster uuid.v4 https://dev.to/galkin/crypto-randomuuid-vs-uuid-v4-47i5?utm_source=dormosheio&utm_campaign=dormosheio
   const uuid = crypto.randomUUID();
   return uuid;
 };
